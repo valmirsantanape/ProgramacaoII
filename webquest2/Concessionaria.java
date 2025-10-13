e427c4d27f01e0e3cb09c1bd38ef0c0c7e538bb5
Victoria Souza <73940090+ylsvictoria@users.noreply.github.com>
2025-10-13 20:35:51 -0300

    Ajuste Concessionaria metodo generico

--- webquest2/Concessionaria.java
+++ webquest2/Concessionaria.java
@@ -1,5 +1,5 @@
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Concessionaria {
     
@@ -7,26 +7,22 @@ public class Concessionaria {
 
         ArrayList<Veiculo> veiculos = new ArrayList<>();
 
-        adicionarCarros(veiculos);
-        adicionarMotos(veiculos);
+        registrarVeiculo(veiculos, new Carro("ABC-1234", "Toyota", "Corolla", 2022, 120000, 4));
+        registrarVeiculo(veiculos, new Carro("XYZ-5678", "Honda", "Civic", 2021, 110000, 4));
+        registrarVeiculo(veiculos, new Carro("DEF-9999", "Volkswagen", "Gol", 2019, 50000, 2));
 
-        System.out.println("===== LISTA DE VEÍCULOS =====\n");
+        registrarVeiculo(veiculos, new Moto("MOT-1122", "Yamaha", "MT-07", 2020, 38000, 689));
+        registrarVeiculo(veiculos, new Moto("MOT-3344", "Honda", "CG 160", 2023, 15000, 162));
+
+
+        System.out.println("===== LISTA DE VEÍCULOS CADASTRADOS =====\n");
         for (Veiculo v : veiculos) {
             v.exibirInformacoes();
             System.out.println("----------------------------");
         }
-
     }
 
-    private static void adicionarCarros(ArrayList<Veiculo> veiculos) {
-        veiculos.add(new Carro("ABC-1234", "Toyota", "Corolla", 2022, 120000, 4));
-        veiculos.add(new Carro("XYZ-5678", "Honda", "Civic", 2021, 110000, 4));
-        veiculos.add(new Carro("DEF-9999", "Volkswagen", "Gol", 2019, 50000, 2));
+   private static void registrarVeiculo(ArrayList<Veiculo> veiculos, Veiculo v) {
+        veiculos.add(v);
     }
-
-    private static void adicionarMotos(ArrayList<Veiculo> veiculos) {
-        veiculos.add(new Moto("MOT-1122", "Yamaha", "MT-07", 2020, 38000, 689));
-        veiculos.add(new Moto("MOT-3344", "Honda", "CG 160", 2023, 15000, 162));
-    }
-        
 }
