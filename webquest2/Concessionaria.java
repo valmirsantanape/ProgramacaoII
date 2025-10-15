8fa22e330adb73b2a0672e33cffe559979481adf
Victoria Souza <73940090+ylsvictoria@users.noreply.github.com>
2025-10-15 15:10:29 -0300

    Adicionando caminhão em Concessionaria.java

--- webquest2/Concessionaria.java
+++ webquest2/Concessionaria.java
@@ -10,10 +10,9 @@ public class Concessionaria {
         registrarVeiculo(veiculos, new Carro("ABC-1234", "Toyota", "Corolla", 2022, 120000, 4));
         registrarVeiculo(veiculos, new Carro("XYZ-5678", "Honda", "Civic", 2021, 110000, 4));
         registrarVeiculo(veiculos, new Carro("DEF-9999", "Volkswagen", "Gol", 2019, 50000, 2));
-
         registrarVeiculo(veiculos, new Moto("MOT-1122", "Yamaha", "MT-07", 2020, 38000, 689));
         registrarVeiculo(veiculos, new Moto("MOT-3344", "Honda", "CG 160", 2023, 15000, 162));
-
+        registrarVeiculo(veiculos, new Caminhao("CAM-5566", "Volvo", "FH 540", 2022, 750000, 25));
 
         System.out.println("===== LISTA DE VEÍCULOS CADASTRADOS =====\n");
         for (Veiculo v : veiculos) {
